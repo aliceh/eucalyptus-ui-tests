48c359d44abd438a6ed3e450ce9406bc87329e6e
Brian Thomason <brian.thomason@gmail.com>
2013-02-06 13:29:09 -0500

    Updated nav module

--- navigation.py
+++ navigation.py
@@ -7,17 +7,32 @@ def click_running(driver):
 def click_keypair(driver):
   driver.find_element_by_xpath("//div[@id='dashboard-netsec-keypair']").click()
 
+def click_keypair_link(driver):
+  driver.find_element_by_link_text("Key Pairs").click()
+
 def click_secgroup(driver):
   driver.find_element_by_xpath("//div[@id='dashboard-netsec-sgroup']").click()
+
+def click_secgroup_link(driver):
+  driver.find_element_by_link_text("Security Groups").click()
 
 def click_volumes(driver):
   driver.find_element_by_xpath("//div[@id='dashboard-storage-volume']").click()
 
+def click_volumes_link(driver):
+  driver.find_element_by_link_text("Volumes").click()
+
 def click_snapshots(driver):
   driver.find_element_by_xpath("//div[@id='dashboard-storage-snapshot']").click()
 
+def click_snapshots_link(driver):
+  driver.find_element_by_link_text("Snapshots").click()
+
 def click_ips(driver):
   driver.find_element_by_xpath("//div[@id='dashboard-netsec-eip']/span").click()
+
+def click_ips_link(driver):
+  driver.find_element_by_link_text("IP Addresses").click()
 
 def click_stopped(driver):
   driver.find_element_by_xpath("//div[@id='dashboard-instance-stopped']/div").click()
@@ -30,3 +45,9 @@ def click_images(driver):
 
 def click_instances(driver):
   driver.find_element_by_link_text("Instances").click()
+
+def click_storage(driver):
+  driver.find_element_by_link_text("Storage").click()
+
+def click_network(driver):
+  driver.find_element_by_link_text("Network & Security").click()
